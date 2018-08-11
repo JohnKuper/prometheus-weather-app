97c1dc86383bdcac38f2eb9d0127152439c2b940
Dmitry Korobeinikov <korobeinikov@yamoney.ru>
2018-08-11 12:27:38 +0300

    Added Places SDK.

--- app/src/main/java/com/kaizendeveloper/testweatherapp/WeatherFeedActivity.kt
+++ app/src/main/java/com/kaizendeveloper/testweatherapp/WeatherFeedActivity.kt
@@ -1,12 +1,36 @@
 package com.kaizendeveloper.testweatherapp
 
-import android.support.v7.app.AppCompatActivity
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+import com.google.android.gms.location.places.ui.PlacePicker
+import kotlinx.android.synthetic.main.activity_weather_feed.button
+
+const val PLACE_PICKER_REQUEST = 1
 
 class WeatherFeedActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_weather_feed)
+
+        button.setOnClickListener {
+            val builder = PlacePicker.IntentBuilder()
+
+            startActivityForResult(builder.build(this), PLACE_PICKER_REQUEST)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == PLACE_PICKER_REQUEST) {
+            if (resultCode == Activity.RESULT_OK) {
+                val place = PlacePicker.getPlace(this, data)
+                val toastMsg = String.format("Place: %s", place.name)
+                Toast.makeText(this, toastMsg, Toast.LENGTH_LONG).show()
+            }
+        }
     }
 }
